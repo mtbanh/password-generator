be8f0f8a3d0c6ec9b8c09f89201a213cd918458d
Mai Banh <65269951+mtbanh@users.noreply.github.com>
2020-06-18 18:05:49 -0700

    created other criteria prompt and make sure that the user select at least 1 special character

--- script.js
+++ script.js
@@ -31,6 +31,25 @@ function promptCriteria(){
       passwordLength = parseInt(passwordLength)
     }
   };
+  // Check to see if one of the above options was chosen, if not loop back
+  while (!(specialCharacters || lowercase || uppercase || numbers)) {
+    // Ask user if they want special characters using a 'confirm()'
+    specialCharacters = confirm("Do you want special characters?");
+
+    // Ask user if they want lowercase using a 'confirm()'
+    lowercase = confirm("Do you want lowercase characters?");
+
+    // Ask user if they want uppercase using a 'confirm()'
+    uppercase = confirm("Do you want uppercase characters?");
+
+    // Ask user if they want numbers using a 'confirm()'
+    numbers = confirm("Do you want numbers?");
+
+    if (!(specialCharacters || lowercase || uppercase || numbers)) {
+      alert("Please select at least one special criteria");
+      continue;
+    }
+  };
 }
 
 // Add event listener to generate button
