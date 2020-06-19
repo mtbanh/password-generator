7e5f67250f4e57f940bcb69ccf91b018b2802612
Mai Banh <65269951+mtbanh@users.noreply.github.com>
2020-06-18 19:24:52 -0700

    edited README file and added a gif of deployed site

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
-# password-generator
-js practice
+# Password Generator
+
+## About the project
+
+This application will generate a random password between 8 and 128 characters. Base on the user input, the password generated can include uppercase, lowercase, numbers and special character. 
+
+---
+
+## Getting started
+Below are the prerequisite understanding and programs that were utilized :
+* Visual Studio Code-click [here](https://code.visualstudio.com/) to a tutorial to install
+* Github repository-click [here](https://help.github.com/en/github/)
+
+---
+
+## Code spotlight
+
+The code snippet below execute an alert until the user input satisfy our condition.
+```js
+{
+    while (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+
+    passwordLength = prompt("Password length: Please choose a number between 8 to 128 ")
+    if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+      alert("Please enter a number between 8 and 128");
+      passwordLength = 0;
+      continue;
+    }
+  };
+}
+```
+
+---
+
+## Demo
+
+
+![Deployed Application](deployed-site.gif)
+
+---
+
+## Deployed link
+
+[Live site](https://mtbanh.github.io/password-generator/)
+---
+
+## Author
+
+**Mai Banh**
+- [Link to Github](https://github.com/mtbanh)
+- [Link to LinkedIn](https://www.linkedin.com/in/mai-banh-311ba6164/)
+
+See also the list of [contributors](https://github.com/your/project/contributors) who participated in this project.
+
+## Acknowledgments
+Thank you to everyone who have helped out, and especially my mentor, Matthew Chen! 
--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function createPassword() {
   writePassword(password);
 }
 
-function promptCriteria(){
+function promptCriteria() {
 
   var passwordLength = 0;
   var specialCharacters = false;
@@ -27,7 +27,7 @@ function promptCriteria(){
   var numbers = false;
 
   while (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
-    // Ask user for the length of password using a 'prompt()'
+
     passwordLength = prompt("Password length: Please choose a number between 8 to 128 ")
     if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
       alert("Please enter a number between 8 and 128");
@@ -38,18 +38,18 @@ function promptCriteria(){
       passwordLength = parseInt(passwordLength)
     }
   };
-  // Check to see if one of the above options was chosen, if not loop back
+
   while (!(specialCharacters || lowercase || uppercase || numbers)) {
-    // Ask user if they want special characters using a 'confirm()'
+
     specialCharacters = confirm("Do you want special characters?");
 
-    // Ask user if they want lowercase using a 'confirm()'
+
     lowercase = confirm("Do you want lowercase characters?");
 
-    // Ask user if they want uppercase using a 'confirm()'
+
     uppercase = confirm("Do you want uppercase characters?");
 
-    // Ask user if they want numbers using a 'confirm()'
+
     numbers = confirm("Do you want numbers?");
 
     if (!(specialCharacters || lowercase || uppercase || numbers)) {
@@ -76,23 +76,27 @@ function generatePassword(criteria) {
   }
 
   if (criteria.lowercase) {
-    // TODO - Add more lowercase
-    // acceptableCharacters = acceptableCharacters.concat(["a", "b", "c", "d", "e", "f", "g", "h", "i"])
+
     acceptableCharacters = acceptableCharacters.concat('abcdefghijklmnopqrstuvwxyz'.split(''));
   }
 
   if (criteria.uppercase) {
-    // TODO - Add more uppercase
-    // acceptableCharacters = acceptableCharacters.concat(["A", "B", "C", "D", "E", "F", "G", "H", "I"])
+
     acceptableCharacters = acceptableCharacters.concat('ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split(''))
 
   }
 
   if (criteria.numbers) {
-    // TODO - Add more numbers
+
     acceptableCharacters = acceptableCharacters.concat(["1", "2", "3", "4", "5", "6", "7", "8", "9"])
   }
+  for (var i = 0; i < criteria.passwordLength; i++) {
 
+    password = password + acceptableCharacters[Math.floor(Math.random() * acceptableCharacters.length)];
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", createPassword);
+  }
+
+  return password
+};
+
+  generateBtn.addEventListener("click", createPassword);
